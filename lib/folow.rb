6b38aec686861c9d3c143f19c675baa5d9e8eb6f
bamss <amadougolfa98@gmail.com>
2019-01-16 15:12:58 +0100

    le premier exercice follow terminé

---
+++ lib/folow.rb
@@ -0,0 +1,21 @@
+require 'twitter'
+
+require 'dotenv'
+Dotenv.load
+
+client = Twitter::REST::Client.new do |config|
+   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+   config.access_token_secret = ENV["TWITTER_ACCES_TOKEN_SECRET"]
+ end
+
+ @client = client
+
+ def folow
+    @client.search("#bonjour_monde", result_type: 'recent').take(20).each do |tweet|
+        @client.follow(tweet.user.screen_name)
+    end
+ end
+
+ folow
